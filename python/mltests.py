21df706dae9996dca295bc431133996014397ebd
John U. Balis <=>
2018-12-23 12:32:34 -0600

    fixed python class error

--- python/mltests.py
+++ python/mltests.py
@@ -4,25 +4,25 @@ import math
 
 
 class Neuron:
-	target_count = 0
-	targets = []
-	potential_weights = []
-	fired = 0
-	activation_potential = 0
-	excitation = 0
+	
 	def __init__(self):
 		self.activation_potential = uniform(0,1)
-	
+		self.target_count = 0
+		self.targets = []
+		self.potential_weights = []
+		self.fired = 0
+		self.activation_potential = 0
+		self.excitation = 0
 
 
 
 
 class Brain:
-	neuron_count = 0
-	neurons = []
+
 	def __init__(self, neuron_count):
 		self.neuron_count = neuron_count
-		for i in range(neuron_count):
+		self.neurons = []
+		for i in range(neuron_count):			
 			self.neurons.append(Neuron())
 	
 	def print_brain(self): # needs testing
@@ -34,6 +34,8 @@ class Brain:
 			print ("NEURON {index}".format(index = index))
 			for i in range(neuron.target_count):
 				print ("TARGET: {target}, WEIGHT: {weight}".format(target = neuron.targets[i],weight = neuron.potential_weights[i]))
+		print('NEURON COUNT RECORD: ' +  str(self.neuron_count))
+		print('NEURON COUNT: ' + str(len(self.neurons)))
 			
 	
 	def sigmoid(self, x):
@@ -43,20 +45,35 @@ class Brain:
 	def hyperbolic_tangent(self, x):
 		return 2*self.sigmoid(2*x)-1
 
+	def verify_network_consistency(self):
+		#print("NEURON COUNT: " + str(self.neuron_count)) 
+		#print("NEURONS LENGTH: " + str(len(self.neurons))) 
+		#assert(self.neuron_count == len(self.neurons))
+		#for neuron in self.neurons:
+		#	for target in neuron.targets:
+		#		print('TARGET: ' + str(target))
+		#		print("NEURON COUNT: " + str(self.neuron_count)) 
+		#		assert(target < self.neuron_count)
+		pass
+		
+
 	def neuron_count_mutation(self, min_input_count, min_output_count, probability, bias):
+		self.verify_network_consistency()
 		if uniform(0,1) < probability:		
 			if uniform(0,1) < bias:
-				self.neuron_count += 1
 				self.neurons.append(Neuron())	#needs neuron init function
+				self.neuron_count  += 1
 			elif self.neuron_count > 1 + min_input_count + min_output_count:
-				self.neuron_count -= 1 
+				self.neuron_count -= 1
 				del self.neurons[self.neuron_count]
 				for neuron in self.neurons:
 					for index, target in enumerate(neuron.targets):
-						if target == self.neuron_count:
+						if target >= self.neuron_count:
 							neuron.targets[index] = randrange(0,self.neuron_count)
+		self.verify_network_consistency()
 
 	def target_mutation(self, max_targets, target_count_probability, target_count_bias, retarget_probability):
+		self.verify_network_consistency()		
 		for i, neuron in enumerate(self.neurons):
 			if  target_count_probability > uniform(0,1):
 				if target_count_bias > uniform(0,1) :
@@ -69,22 +86,30 @@ class Brain:
 						neuron.target_count -= 1
 						del neuron.targets[neuron.target_count]
 						del neuron.potential_weights[neuron.target_count]
+
+
 				if retarget_probability > uniform(0,1) and neuron.target_count > 0:
 					neuron.targets[randrange(0,neuron.target_count)] = randrange(self.neuron_count)
+		self.verify_network_consistency()
 					
 	def potential_weights_mutation(self, probability, strength):
+		self.verify_network_consistency()
 		for neuron in self.neurons:
 			if neuron.target_count > 0:
 				mutations = randrange(0, neuron.target_count)
 				for i in range(mutations):
 					if uniform(0,1) < probability:
 						neuron.potential_weights[randrange(0,neuron.target_count)] += ((uniform(0,1) *2)-1)*strength
+		self.verify_network_consistency()
 	def threshold_mutation(self, probability, strength):
+		self.verify_network_consistency()		
 		for neuron in self.neurons:
 			if uniform(0,1) < probability:
 				neuron.activation_potential += ((uniform(0,1) *2)-1)*strength
+		self.verify_network_consistency()
 
 	def neuron_swap_mutation(self, probability):
+		self.verify_network_consistency()
 		index1 = randrange(0, self.neuron_count -1) + 1 
 		index2 = randrange(0, self.neuron_count -1) + 1 
 		while index2 == index1:
@@ -92,14 +117,17 @@ class Brain:
 		temp = self.neurons[index1]
 		self.neurons[index1] = self.neurons[index2]
 		self.neurons[index2] = temp
+		self.verify_network_consistency()
 	
 	def default_mutation(self, min_input_count, min_output_count):
+		self.verify_network_consistency()		
 		self.neuron_swap_mutation(.1)
 		self.neuron_count_mutation(min_input_count, min_output_count, .125, .5)
 		self.target_mutation(10,.25,.5,.25)
 		self.potential_weights_mutation(1,.1)
 		self.threshold_mutation(1,.1)
-
+		self.verify_network_consistency()
+	
 	def input(self, inputs):
 		self.neurons[0].excitation = self.neurons[0].activation_potential + 1
 		for i in range(1, len(inputs) + 1 ):
@@ -111,14 +139,21 @@ class Brain:
 		assert not (self.neuron_count < 1 + len(inputs) + output_length)
 		self.input(inputs)
 		sums  = [0] * self.neuron_count
-		print(len(sums))
+		#print(len(sums))
 		for i in range(self.neuron_count):
 			self.neurons[i].fired = 0
 			if self.hyperbolic_tangent(self.neurons[i].excitation) > self.neurons[i].activation_potential:
 				for c in range (self.neurons[i].target_count):
-					print(self.neurons[i].targets)
-					print(self.neurons[i].potential_weights)
+					#print(len(self.neurons[i].targets))
+					#print(len(self.neurons[i].potential_weights))
+					#print(self.neurons[i].targets[c])
+					#print('TARGET COUNT: ' + str(self.neurons[i].target_count))
+					#print('TARGET INDEX: ' + str(c))
+					#print('TARGET: ' + str(self.neurons[i].targets[c]))
+					#print('SUMS: ' + str(len(sums)))
+					assert(self.neurons[i].targets[c] < self.neuron_count)
 					sums[self.neurons[i].targets[c]] += self.neurons[i].potential_weights[c]
+					#sums[1] += self.neurons[i].potential_weights[c]
 				self.neurons[i].fired = 1
 				self.neurons[i].excitation = self.neurons[i].excitation/2.0
 		
@@ -164,6 +199,7 @@ class Brain:
 def learn_xor():
 
 	best_brain = Brain(4)
+	best_brain.verify_network_consistency()
 	best_score = 0
 	
 	
@@ -171,13 +207,22 @@ def learn_xor():
 
 		score = 0
 		
-		mutant = copy.deepcopy(best_brain)
+		mutant = copy.deepcopy(best_brain) #hELLO ERROR
+		#mutant.print_brain()
+		#best_brain.print_brain()	
+		best_brain.verify_network_consistency()	
+		mutant.verify_network_consistency()
 		mutant.default_mutation(2,1)
+		mutant.verify_network_consistency()
 		test_instance = copy.deepcopy(mutant)
+		test_instance.verify_network_consistency()
 		score = test_instance.evaluate_xor_performance()
 		if score > best_score:
+			print(score)
 			best_score = score
-			best_brain = mutant
+			mutant.verify_network_consistency()
+			best_brain = copy.deepcopy(mutant)
+			best_brain.verify_network_consistency()
 	
 
 	return best_brain
